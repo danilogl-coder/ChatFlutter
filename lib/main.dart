da93ac9c1dfbf73600551ec43937c921764f0595
danilo <daniloglcomercial@gmail.com>
2023-07-25 12:48:52 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import "check_user.dart";
 import "home_page.dart";
 
 void main() async {
-  // Esse comando esta garantindo inicialização correta antes do flutter inciar
+  // Esse comando esta garantindo inicialização correta antes do flutter iniciar
   WidgetsFlutterBinding.ensureInitialized();
   // Esse comando iniciliza o servidor
   await Firebase.initializeApp(
@@ -15,10 +15,71 @@ void main() async {
       options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(MyApp());
-  await FirebaseFirestore.instance
-      .collection("col")
-      .doc("doc")
-      .set({"texto": "daniel"});
+  //FirebaseFirestore.instance cria uma instancia do App inteiro set Lê e cria
+  FirebaseFirestore.instance
+      .collection("Mensagens")
+      .doc("3P0p2tXUswKKb8056QuG")
+      .set({"texto": "Hola", "from": "Diego", "read": true});
+  //Atualiza coisa especifica
+  FirebaseFirestore.instance
+      .collection("Mensagens")
+      .doc("3P0p2tXUswKKb8056QuG")
+      .update({"read": false});
+  //Podemos criar subcoleções
+  FirebaseFirestore.instance
+      .collection("Mensagens")
+      .doc("3P0p2tXUswKKb8056QuG")
+      .collection("arquivos")
+      .doc()
+      .set({"arqname": "foto.png"});
+
+  //Lendo dados
+
+  //Obtendo todos os documentos
+  QuerySnapshot snapshot =
+      await FirebaseFirestore.instance.collection("Mensagens").get();
+  snapshot.docs.forEach((d) {
+    print(d.data());
+  });
+
+  //Obtendo um documento
+  DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+      .collection("Mensagens")
+      .doc("3P0p2tXUswKKb8056QuG")
+      .get();
+
+  print(documentSnapshot.data());
+
+  //Obtendo todos os documentos com id
+  QuerySnapshot snapshot2 =
+      await FirebaseFirestore.instance.collection("Mensagens").get();
+  snapshot2.docs.forEach((d) {
+    print(d.data());
+    print(d.id);
+  });
+
+  //Adicionando campo por referencia
+  QuerySnapshot snapshot3 =
+      await FirebaseFirestore.instance.collection("Mensagens").get();
+  snapshot3.docs.forEach((d) {
+    d.reference.update({"lido": false});
+  });
+
+  //Update/lendo em tempo real
+  FirebaseFirestore.instance.collection("Mensagens").snapshots().listen((dado) {
+    dado.docs.forEach((e) {
+      print(e.data());
+    });
+  });
+
+  //Update/lendo documento unico
+  FirebaseFirestore.instance
+      .collection("Mensagens")
+      .doc("3P0p2tXUswKKb8056QuG")
+      .snapshots()
+      .listen((e) {
+    print(e.data());
+  });
 }
 
 class MyApp extends StatelessWidget {
