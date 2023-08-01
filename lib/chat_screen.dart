f117b775dca5cdd45984f23b3b031438dd9354cd
Danilo Silva <daniloglcomercial@gmail.com>
2023-07-31 22:54:32 -0300

    .

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,27 +2,75 @@ import 'dart:io';
 
 import 'package:chatflutter/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  ChatScreen({super.key});
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 
-  void _getUser(){
-    try {
-      
-    } catch (e) {
-      
-    }
-  }
+ 
 
 }
 
-void _sendMessage({String? text, File? imgFile}) async {
-  Map<String, dynamic> data = {};
+
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  //Cadastro com google
+ final GoogleSignIn googleSignIn = GoogleSignIn();
+ //Barra de erro
+ final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+ User ?_currentUser;
+  @override
+  void initState()
+  {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((user) { 
+      setState(() {
+       _currentUser = user;
+      });
+    });
+  }
+
+  Future _getUser() async {
+    if(_currentUser != null){
+      return _currentUser;
+    }
+    try {
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount!.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken);
+
+      final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+      final User ?user = authResult.user;
+      return user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+
+
+  void _sendMessage({String? text, File? imgFile}) async {
+  final User user = await _getUser();
+
+  if(user == null){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Não foi possível fazer o login. tente novamente"), backgroundColor: Colors.red,));
+  }
+  Map<String, dynamic> data = {
+    "uid" : user.uid,
+    "senderNamer" : user.displayName,
+    "senderPhotoURL" : user.photoURL
+  };
 
   //Salvando imagem
   if (imgFile != null) {
@@ -44,10 +92,11 @@ void _sendMessage({String? text, File? imgFile}) async {
   FirebaseFirestore.instance.collection("messeges").add(data);
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Ola"),
         elevation: 0,
