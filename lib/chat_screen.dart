e1d876e036dfaa19cfd0e5bbc28b378474122b37
Danilo <daniloglcomercial@gmail.com>
2023-08-01 23:42:11 -0300

    .

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -12,45 +12,41 @@ class ChatScreen extends StatefulWidget {
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
-
- 
-
 }
 
-
-
 class _ChatScreenState extends State<ChatScreen> {
-
   //Cadastro com google
- final GoogleSignIn googleSignIn = GoogleSignIn();
- //Barra de erro
- final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
- User ?_currentUser;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  //Barra de erro
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  User? _currentUser;
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
-    FirebaseAuth.instance.authStateChanges().listen((user) { 
+    FirebaseAuth.instance.authStateChanges().listen((user) {
       setState(() {
-       _currentUser = user;
+        _currentUser = user;
       });
     });
   }
 
   Future _getUser() async {
-    if(_currentUser != null){
+    if (_currentUser != null) {
       return _currentUser;
     }
     try {
-      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount!.authentication;
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
 
       final AuthCredential credential = GoogleAuthProvider.credential(
-      idToken: googleSignInAuthentication.idToken,
-      accessToken: googleSignInAuthentication.accessToken);
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
 
-      final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
-      final User ?user = authResult.user;
+      final UserCredential authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final User? user = authResult.user;
       return user;
     } catch (e) {
       print(e);
@@ -58,40 +54,40 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-
-
   void _sendMessage({String? text, File? imgFile}) async {
-  final User user = await _getUser();
+    final User? user = await _getUser();
 
-  if(user == null){
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Não foi possível fazer o login. tente novamente"), backgroundColor: Colors.red,));
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Não foi possível fazer o login. tente novamente"),
+        backgroundColor: Colors.red,
+      ));
+    }
+    Map<String, dynamic> data = {
+      "uid": user!.uid,
+      "senderNamer": user.displayName,
+      "senderPhotoURL": user.photoURL
+    };
+
+    //Salvando imagem
+    if (imgFile != null) {
+      UploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+
+      //Provalvel erro falta do onComplete
+      TaskSnapshot taskSnapshot = await task;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if (text != null) {
+      data['text'] = text;
+    }
+
+    FirebaseFirestore.instance.collection("messeges").add(data);
   }
-  Map<String, dynamic> data = {
-    "uid" : user.uid,
-    "senderNamer" : user.displayName,
-    "senderPhotoURL" : user.photoURL
-  };
-
-  //Salvando imagem
-  if (imgFile != null) {
-    UploadTask task = FirebaseStorage.instance
-        .ref()
-        .child(DateTime.now().millisecondsSinceEpoch.toString())
-        .putFile(imgFile);
-
-    //Provalvel erro falta do onComplete
-    TaskSnapshot taskSnapshot = await task;
-    String url = await taskSnapshot.ref.getDownloadURL();
-    data['imgUrl'] = url;
-  }
-
-  if (text != null) {
-    data['text'] = text;
-  }
-
-  FirebaseFirestore.instance.collection("messeges").add(data);
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -103,27 +99,32 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
       body: Column(
         children: [
-          Expanded(child: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance.collection('messeges').snapshots(),
+          Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection('messeges').snapshots(),
             builder: (context, snapshot) {
-              switch(snapshot.connectionState){
+              switch (snapshot.connectionState) {
                 case ConnectionState.none:
                 case ConnectionState.waiting:
-                return Center(
-                  child:CircularProgressIndicator(),
-                );
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
                 default:
-                List<DocumentSnapshot> documents = snapshot.data!.docs.reversed.toList();
-                return ListView.builder(
-                  reverse: true,
-                  itemCount: documents.length,
-                  itemBuilder: (context, index){
-                    return ListTile(
-                      title: Text(documents[index].get('text')),
-                    );
-                  },);
+                  List<DocumentSnapshot> documents =
+                      snapshot.data!.docs.reversed.toList();
+                  return ListView.builder(
+                    reverse: true,
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(documents[index].get('text')),
+                      );
+                    },
+                  );
               }
-            },)),
+            },
+          )),
           TextComposer(sendMessage: _sendMessage),
         ],
       ),
