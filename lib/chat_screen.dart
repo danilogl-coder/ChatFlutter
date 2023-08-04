52aafc4f6184896cd346da009e6f6299b1ba1193
Danilo Silva <daniloglcomercial@gmail.com>
2023-08-03 22:11:46 -0300

    .

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -78,7 +78,7 @@ class _ChatScreenState extends State<ChatScreen> {
     if (imgFile != null) {
       UploadTask task = FirebaseStorage.instance
           .ref()
-          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .child(user.uid + DateTime.now().millisecondsSinceEpoch.toString())
           .putFile(imgFile);
 
       setState(() {
